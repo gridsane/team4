174e3fd468b39241821bb6da14365c865e852ef3
grid <gridsane@gmail.com>
2014-12-13 11:29:38 +0500

    карта для города без метки

--- src/views/forecast_map.js
+++ src/views/forecast_map.js
@@ -6,11 +6,24 @@ var ForecastMapView = TabPaneView.extend({
 
     initialize: function (options) {
         this.initializeTabs(options.state);
-        this.model.on('change', this.render, this);
+        this.state = options.state;
+        this.state.on('change:locality', this.render, this);
     },
 
     render: function() {
-        this.$el.html("map!");
+        var self = this;
+        var locality = this.state.get('locality');
+
+        if (!this.map) {
+            ymaps.ready(function () {
+                self.map = new ymaps.Map(self.el, {
+                    center: [locality.lat, locality.lon],
+                    zoom: 10
+                });
+            });
+        } else {
+            this.map.setCenter([locality.lat, locality.lon]);
+        }
     }
 });
 
