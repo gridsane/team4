9f476c83da6229a902bb03c6a7cc26ee4c79314b
grid <gridsane@gmail.com>
2014-12-13 13:39:51 +0500

    метка и всплывающее окно для текущего города

--- src/views/forecast_map.js
+++ src/views/forecast_map.js
@@ -1,4 +1,6 @@
 var TabPaneView = require('./tab_pane');
+var balloonTemplate = require('../templates/forecast_map_balloon.hbs');
+var iconTemplate = require('../templates/forecast_map_icon.hbs');
 
 var ForecastMapView = TabPaneView.extend({
 
@@ -10,6 +12,21 @@ var ForecastMapView = TabPaneView.extend({
         this.state.on('change:locality', this.render, this);
     },
 
+    createPlacemark: function (locality, fact) {
+        return new ymaps.Placemark([locality.lat, locality.lon], {
+            balloonContent: balloonTemplate({
+                locality: locality,
+                fact: fact.toJSON()
+            }),
+            iconContent: iconTemplate(fact.toJSON()),
+        }, {
+            iconLayout: 'default#imageWithContent',
+            iconImageHref: '/assets/images/map_placemark.png',
+            iconImageSize: [68, 42],
+            iconImageOffset: [-20, -42],
+        })
+    },
+
     render: function() {
         var self = this;
         var locality = this.state.get('locality');
@@ -18,11 +35,16 @@ var ForecastMapView = TabPaneView.extend({
             ymaps.ready(function () {
                 self.map = new ymaps.Map(self.el, {
                     center: [locality.lat, locality.lon],
-                    zoom: 10
+                    zoom: locality.zoom
                 });
+
+                self.map.geoObjects.add(self.createPlacemark(locality, self.model));
             });
         } else {
+            this.map.geoObjects.removeAll();
+            this.map.geoObjects.add(self.createPlacemark(locality, self.model));
             this.map.setCenter([locality.lat, locality.lon]);
+            this.map.setZoom(locality.zoom);
         }
     }
 });
